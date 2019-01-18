1f4f4e6e336fe7342c9047b98a0f2eb5ce8c5248
elliot <elliotgcraig@icloud.com>
2019-01-18 14:33:04 -0700

    responsea1 working well

--- src/Components/MainComponent/ScenarioAndResponses/Responses/Responses/ResponseA1.js
+++ src/Components/MainComponent/ScenarioAndResponses/Responses/Responses/ResponseA1.js
@@ -17,20 +17,45 @@ class ResponseA1 extends Component {
 
     handleButtonClick(){
         this.setState({
-            submitedInput: this.state.userInput
+            submitedInput: this.state.userInput,
+            userInput: ''
         })
         console.log(this.state.submitedInput)
+    }
+
+    clearSumbitedInput(){
+        this.setState({
+            submitedInput: ''
+        })
     }
 
     render(){
         return(
             <div className='response-a-one' >
-                <input 
-                    onChange={(e)=>this.handleUserInput(e.target.value)}
-                />
-                <button onClick={()=>this.handleButtonClick()}>abc</button>
+                {this.state.submitedInput.length ? (
+                    <div>
+                        <p>{this.state.submitedInput}</p>
+                        <button onClick={()=>this.clearSumbitedInput()}>reset</button>
+                    </div>
+                ) : (
+                    <div>
+                        <input 
+                        onChange={(e)=>this.handleUserInput(e.target.value)}
+                        />
+                        <button onClick={()=>this.handleButtonClick()}>abc</button>
+                    </div>
+                )
+                }
             </div>
         ) 
+        // return(
+        //     <div className='response-a-one' >
+        //         <input 
+        //             onChange={(e)=>this.handleUserInput(e.target.value)}
+        //         />
+        //         <button onClick={()=>this.handleButtonClick()}>abc</button>
+        //     </div>
+        // ) 
     }
 }
 
