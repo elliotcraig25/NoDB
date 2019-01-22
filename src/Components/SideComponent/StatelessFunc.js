3529356d6bfd8a4a401831544a8fd5dcbff63d96
elliot <elliotgcraig@icloud.com>
2019-01-22 10:09:14 -0700

    typo

--- src/Components/SideComponent/StatelessFunc.js
+++ src/Components/SideComponent/StatelessFunc.js
@@ -3,7 +3,7 @@ import React from 'react';
 const StatlessFunc = (props) => {
     return (
         <div>
-            <p>Hey there {props.word}, how's it goin? Welcome to the Make a Sentence website! Would your like a quick tutorial?</p>
+            <p>Hey there {props.word}, how's it goin? Welcome to the Make a Sentence website! Would you like a quick tutorial?</p>
         </div>
     )
 }
